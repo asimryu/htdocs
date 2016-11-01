9cd650793018c8d419b19c4334a877fd6682f2bd
ryu seunglyul <asimryu@gmail.com>
2016-11-01 15:01:49 +0900

    animation
    animation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-// script.js
-//console.log(new Date());
+var slides = $("header > img");
+var max = slides.length - 1;
+var sno = 0;
+
+$("header").on("click",function(){
+	$( slides[sno] ).animate({"left":"940px"},1000,function(){
+		$(this).css({"left":"-940px"});
+	});
+	sno++;
+	if( sno > max ) sno = 0;
+	$( slides[sno] ).animate({"left":"0"},1000);
+});
+
+var timer = setInterval(function(){
+	$("header").click();//트리거(방아쇠)
+},2000);
